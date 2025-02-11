d18379cc9bb4b68c0f8928b3f0681b5229e453a1
DidonCode <perrier.richardj@gmail.com>
2025-02-11 16:40:27 +0100

    eslintEnd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,25 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+const isBuilding = process.env.NODE_ENV === 'production';
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      }
+    }
+  },
   {
     rules: {
       'no-alert': 'error',
       'no-ternary': 'warn',
       'no-console': 'warn',
       'no-warning-comments':
-        process.env.NODE_ENV === 'production' ? 'off' : 'error',
+      isBuilding === 'production' ? 'off' : 'error',
     },
   },
   pluginJs.configs.recommended,
