2f1f457ed9ddaad43ec631dcfec6fd4fcc434512
DidonCode <perrier.richardj@gmail.com>
2025-02-11 16:19:03 +0100

    eslintBuildProd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,15 @@ import tseslint from 'typescript-eslint';
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
-  { rules : {"no-alert": "error", "no-ternary" : "warn", "no-console" : "warn", "no-warning-comments" : process.env.NODE_ENV === 'production' ? "off" : "error"}},
+  {
+    rules: {
+      'no-alert': 'error',
+      'no-ternary': 'warn',
+      'no-console': 'warn',
+      'no-warning-comments':
+        process.env.NODE_ENV === 'production' ? 'off' : 'error',
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
