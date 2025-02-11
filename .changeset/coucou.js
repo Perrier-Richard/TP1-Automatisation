38b1610de51d9d55fd331c91b5684882978632d0
DidonCode <perrier.richardj@gmail.com>
2025-02-11 11:19:21 +0100

    prettierExec

--- .changeset/coucou.js
+++ .changeset/coucou.js
@@ -1,14 +1,14 @@
-function coucou(){
+function coucou() {
   console.log("Coucou, c'est moi, le changement !");
 }
 coucou();
 
-function coucouPerso(name){
+function coucouPerso(name) {
   console.log(`Coucou, ${name}!`);
 }
 coucouPerso('PERRIER');
 
-function coucouQuote(){
-  console.log("Coucou, je préfère les quotes simples !");
+function coucouQuote() {
+  console.log('Coucou, je préfère les quotes simples !');
 }
 coucouQuote();
